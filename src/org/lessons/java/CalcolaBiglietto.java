bf2aeb41e9d57a6078d46921e2d044e0bd65ba2c
AntonelloStefania <antonelloste94@gmail.com>
2023-11-09 16:46:17 +0100

    esercizio completo

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -14,12 +14,22 @@ public class CalcolaBiglietto {
 		System.out.print("la tua eta': ");
 		String strEta = in.nextLine();
 		int intEta = Integer.valueOf(strEta);
-		
-		System.out.println(intKm);
-		System.out.println(intEta);
-		
+				
+		in.close();
 		double price = intKm * 0.21;
 		
-		System.out.println(price);
+		System.out.println("il prezzo intero e'" + price + "€");
+		double newPrice = 0;
+		if(intEta < 18) {
+			 newPrice = price * 0.8;
+			
+		}else if (intEta > 65) {
+			 newPrice = price * 0.6;
+		} else {
+			 newPrice = price;
+		}
+		
+		
+		System.out.println("il prezzo del tuo biglietto e'" + newPrice + "€");
 	}
 }
